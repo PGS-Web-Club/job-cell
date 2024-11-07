4f8da70dce2acce41605a8e8dc5d2deaad4e3ce2
Chief-spartan-117 <shresthasagar861@gmail.com>
2024-11-07 08:35:40 +0545

    Schema Updated

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,9 +2,11 @@ import express from "express";
 const router = express.Router();
 
 import indexRoute from "./users/index.routes";
+import userRoute from "./users/user.routes";
 import adminRoute from "./admin/adminIndex.routes";
 
 router.use(indexRoute);
+router.use(userRoute);
 router.use(adminRoute);
 
 export default router;
